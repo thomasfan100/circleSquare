ffced29d5553dce84eed127ccabccf7c76f8ce21
Thomas Fan <thomasfan100@gmail.com>
2021-09-11 11:21:50 -0500

    links to a music page

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@app.route('/music/')
+def music():
+  return render_template('music.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
