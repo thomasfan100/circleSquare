a08c6639d6dc61fd4e6135657e1177d1c9d93134
Keagan Eng <grace.eng02@gmail.com>
2021-09-11 15:42:43 -0500

    fix images on buttons, create nav-bar

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    
-@app.route('/music/')
-def music():
-    return render_template('music.html')
 
 @app.route('/newSkill/')
 def newSkill():
     return render_template('newSkill.html')
+
+@app.route('/toDo/')
+def toDo():
+    return render_template('toDo.html')
+    
+@app.route('/music/')
+def music():
+    return render_template('music.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
