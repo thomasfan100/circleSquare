ad94ff1eac92ff179758cc73a1f5c7ff20c09355
Keagan Eng <grace.eng02@gmail.com>
2021-09-11 22:39:20 -0500

    get new skill working in nav-bar

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random;
+import webbrowser;
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
@@ -8,7 +10,17 @@ def index():
 
 @app.route('/newSkill/')
 def newSkill():
-    return render_template('newSkill.html')
+    sites = [
+        "https://www.chess.com/learn-how-to-play-chess", 
+        "https://www.delicious.com.au/recipes/collections/gallery/60-healthy-dinners-you-can-cook-in-30-minutes/1vo4q819", 
+        "https://www.wordstream.com/blog/ws/2019/12/31/basic-website",
+        "https://musescore.com/",
+        "https://www.pagat.com/"]
+
+    randomNumber = random.randint(0, 4)
+    rand = sites[randomNumber]
+    webbrowser.open(rand)
+    return render_template('index.html')
 
 @app.route('/toDo/')
 def toDo():
